bb05cf5f303d59ad15a2d040c3b4a492f11d5432
Yoavelko <yoavelkobi889@gmail.com>
2023-08-07 13:13:25 +0300

    pacpac

--- src/assets/components/statistics-songbox/StatisticsSongbox.jsx
+++ src/assets/components/statistics-songbox/StatisticsSongbox.jsx
@@ -1,19 +1,22 @@
 import './StatisticsSongbox.css'
 
-function StatisticsSongbox(params) {
+function StatisticsSongbox({value}) {
+
+    let title
+
+    if (value && value?.name?.length > 20) {
+        title = value.name.slice(0, 26) + '...'
+    }
     
     return (
         <div id='statistics-songbox-container'>
             <div id='songbox-img-container'>
-                IMG
-                <img src="" alt="" />
+                <img src={value.img} alt="" />
             </div>
             <div id='songbox-detail-container'>
-                <div id='songbox-video-name'>שם השיר</div>
+                <div id='songbox-video-name'>{title}</div>
                 <div id='songbox-small-row-container'>
-                    <div className='songbox-small-row'>אומן</div>
-                    <div className='dots'>&#x2022;</div>
-                    <div className='songbox-small-row'>נשלח ב-</div>
+                    <div className='songbox-small-row'>{value.artist}</div>
                 </div>
             </div>
         </div>
--- src/assets/components/statistics/Statistics.jsx
+++ src/assets/components/statistics/Statistics.jsx
@@ -1,9 +1,47 @@
 import './Statistics.css'
 import StatisticsSongbox from '../statistics-songbox/StatisticsSongbox'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import cookies from 'js-cookie'
+import { estabBest } from '../../../utils/Establishment'
+import { getPlaylist } from '../../../utils/Establishment'
 
 function Statistics() {
 
     const topten = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const [best, setBest] = useState()
+    const [date, setDate] = useState('06/08/2023')
+    const [playlist, setPlaylist] = useState()
+
+    function handleSelect(event) {
+        const selectedValue = event.target.value
+
+        if (selectedValue === 'overall') {
+            axios.post(estabBest, {
+                establishment: cookies.get('establishment'),
+                splice: 10
+            })
+            .then((res) => {
+                setBest(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+        }
+    }
+
+    useEffect(() => {
+        axios.post(getPlaylist, {
+            establishment: cookies.get('establishment'),
+            date: date
+        })
+        .then ((res) => {
+            setPlaylist(res.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }, [date])
 
     return (
         <div id='statistics-container'>
@@ -12,16 +50,16 @@ function Statistics() {
                 <div id='overall-conversion'>אחוז המרה: %X</div>
                 <div id='top-ten-select'>
                     החמים ביותר
-                    <select name="" id="">
-                        <option value="">היום</option>
-                        <option value="">השבוע</option>
-                        <option value="">החודש</option>
-                        <option value="">בכל הזמנים</option>
+                    <select name="" id="" onChange={handleSelect}>
+                        <option value="today">היום</option>
+                        <option value="week">השבוע</option>
+                        <option value="month">החודש</option>
+                        <option value="overall">בכל הזמנים</option>
                     </select>
                 </div>
                 <div id='hottest-container'>
                     {
-                        topten && topten.map((value, index) => {
+                        best && best.map((value, index) => {
                             return <StatisticsSongbox key={index} value={value} />
                         })
                     }
@@ -49,7 +87,17 @@ function Statistics() {
                             <div>הפלייליסט של DD/MM</div>
                             <button>ייבא פלייליסט</button>
                         </div>
-                        <div id='daily-playlisy-container'></div>
+                        <div id='daily-playlisy-container'>
+                            {
+                                playlist && playlist.map((value, index) => {
+                                    return (
+                                        <div id='single-playlist-song'>
+                                            {value.name} - {value.timeRequested}
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
