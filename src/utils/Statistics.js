1fe17ed81d682968a536d85a7029a23e6dda8737
Yoavelko <yoavelkobi889@gmail.com>
2023-08-07 16:12:43 +0300

    statistics2

--- src/utils/Statistics.js
+++ src/utils/Statistics.js
@@ -1,2 +1,3 @@
 export const HOST = import.meta.env.DEV ? "http://localhost:3000/statistics" : import.meta.env.VITE_SERVER + '/statistics';
 export const getPlaylist = `${HOST}/getPlaylist`;
+export const conversionRate = `${HOST}/conversionRate`;
