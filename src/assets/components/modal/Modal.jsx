10f1f4ecf29153a2ac1f9f919b616ae3fde0b6e3
Yoavelko <yoavelkobi889@gmail.com>
2023-08-08 09:37:09 +0300

    modal loader

--- src/assets/components/modal/Modal.jsx
+++ src/assets/components/modal/Modal.jsx
@@ -5,15 +5,18 @@ import { sendSong } from '../../../utils/UserRoutes'
 import { SocketContext } from '../../../contexts/SocketContext'
 import cookies from 'js-cookie'
 import back from './../../../media/back.png'
+import Loader from '../loader/Loader'
 
 function Modal({ onClose, modalContent }) {
 
     const [checked, setChecked] = useState(false)
     const { socket } = useContext(SocketContext);
+    const [loader, setLoader] = useState(false)
 
     function handleRequest() {
 
         if (checked === true) {
+            setLoader(true)
             axios.patch(sendSong, {
                 establishment: cookies.get('establishment'),
                 today: modalContent.today,
@@ -29,6 +32,7 @@ function Modal({ onClose, modalContent }) {
                     console.log(`sent: ${modalContent.name}`);
                     socket.emit('test', res.data, cookies.get('establishment'));
                     onClose()
+                    setLoader(false)
                     alert('השיר נשלח בהצלחה')
                 })
                 .catch((err) => {
@@ -45,30 +49,37 @@ function Modal({ onClose, modalContent }) {
     return (
         <div id='modal-outer-container'>
             <div id='modal-container'>
-                <div id='close-modal'>
-                    <img src={back} alt="" onClick={onClose} />
-                </div>
-                <div dir='rtl' id='your-choice'>השיר שבחרת:</div>
-                <div id='modal-img-container'>
-                    <img src={modalContent.img} alt="" />
-                </div>
-                <div id='modal-content-container'>
-                    <div id='modal-detail-container'>
-                        <div id='modal-video-name'>{modalContent.name}</div>
-                        <div id='modal-small-row-container'>
-                            <div className='modal-small-row'>{modalContent.artist}</div>
-                            <div className='dots'>&#x2022;</div>
-                            <div className='modal-small-row'>הועלה בשנת {modalContent.uploaded}</div>
-                        </div>
-                    </div>
-                </div>
-                <div id='term-of-use-container' dir='rtl'>
-                    <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)} />
-                    <div>&nbsp;אני מאשר את תנאי השימוש</div>
-                </div>
-                <div>
-                    <button id='modal-send-button' onClick={handleRequest}>שלח</button>
-                </div>
+                {
+                    loader ?
+                        <Loader />
+                        :
+                        <>
+                            <div id='close-modal'>
+                                <img src={back} alt="" onClick={onClose} />
+                            </div>
+                            <div dir='rtl' id='your-choice'>השיר שבחרת:</div>
+                            <div id='modal-img-container'>
+                                <img src={modalContent.img} alt="" />
+                            </div>
+                            <div id='modal-content-container'>
+                                <div id='modal-detail-container'>
+                                    <div id='modal-video-name'>{modalContent.name}</div>
+                                    <div id='modal-small-row-container'>
+                                        <div className='modal-small-row'>{modalContent.artist}</div>
+                                        <div className='dots'>&#x2022;</div>
+                                        <div className='modal-small-row'>הועלה בשנת {modalContent.uploaded}</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div id='term-of-use-container' dir='rtl'>
+                                <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)} />
+                                <div>&nbsp;אני מאשר את תנאי השימוש</div>
+                            </div>
+                            <div>
+                                <button id='modal-send-button' onClick={handleRequest}>שלח</button>
+                            </div>
+                        </>
+                }
             </div>
         </div>
     )
